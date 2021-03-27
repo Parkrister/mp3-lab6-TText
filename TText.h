6c97b8656a30f5c4788938d68e7d0fc2f44c7ed3
Sergey Tsarev <34401022+Parkrister@users.noreply.github.com>
2021-03-27 14:23:48 +0300

    Add files via upload

--- TText.h
+++ TText.h
@@ -3,16 +3,24 @@
 #include <string.h>
 #include "stack.h"
 #include <iostream>
-#include <fstream>
+#include <sstream>
 using namespace std;
 
 const size_t MaxLen = 80;
+
+class TText;
+class TTextLink;
+struct TMem {
+	TTextLink* pFirst, * pLast, * pFree;
+};
 
 class TTextLink
 {
 public:
 	TTextLink* pNext, * pDown;
 	char str[MaxLen];
+	static TMem mem;
+	static InitMem(int size = 100);
 	TTextLink(char* s = nullptr, TTextLink* pn = nullptr, TTextLink* pd = nullptr) {
 		if (s == nullptr)
 			str[0] = '\0';
@@ -100,24 +108,25 @@ class TText {
 
 	// Запись в файл
 	void Save(char* fn) {
-		ofstream ost(fn);
+		ofstream ost;
+		ost.open(char);
 		SaveRec(pFirst, ost);
-		ost.close();
+		ofs.Close();
 	}
 	void SaveRec(TTextLink* t, ofstream& ost) {
 		if (t != NULL) {
-			ost << t->str << '\n';
+			ost << y-> << '\n';
 			if (t->pDown != NULL) {
-				ost << '{\n';
+				ost << "{\n";
 				SaveRec(t->pDown, ost);
-				ost << '}\n';
+				ost << "}\n";
 			}
 			if (t->pNext != NULL)
 				SaveRec(t->pNext, ost);
 		}
 	}
 
-	// Чтение из файла
+	// чтение из файла
 	void Read(char* fn) {
 		ifstream ifs(fn);
 		pFirst = ReadRec(ifs);
@@ -129,6 +138,45 @@ class TText {
 		char Buff[MaxLen];
 		while (!ifs.eof()) {
 			Buff = ifs.getline(Buff, MaxLen, '\n');
+			if (Buff[0] == '}')
+				break;
+			else if (Buff[0] == '{')
+				pC->pDown = ReadRec(ifs);
+			else {
+				TTextLink* tmp = new TTextLink(Buff);
+				if (pC == NULL)
+					pF = pC = tmp;
+				else {
+					pC->pNext = tmp;
+					pC = pC->pNext;
+				}
+			}
 		}
+		return pF;
+	}
+
+	void Reset() {
+		if (pFirst) {
+			st.Clear();
+			pCurr = pFirst;
+			st.push(pFirst);
+			if (pCurr->pNext)
+				st.push(pCurr->pNext);
+			if (pCurr->pDown)
+				st.push(pCurr->pDown);
+		}
+	}
+
+	void GoNext() {
+		pCurr = st.pop();
+		if (pCurr !- pFirst) {
+			if (pCurr->pNext)
+				st.push(pCurr->pNext);
+			if (pCurr->pDown)
+				st.push(pCurr->pDown);
+		}
+	}
+	bool IsEnd() {
+		return st.IsEmpty();
 	}
 };
